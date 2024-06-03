50ee2b5d22a7811367cd8261e769d657bd59f9fa
Frederik Mbius Rygaard <fmry@pop-os.localdomain>
2024-06-03 22:12:10 +0200

    Update

--- runtime_geodesics.py
+++ runtime_geodesics.py
@@ -24,7 +24,7 @@ import pickle
 #argparse
 import argparse
 
-from typing import List
+from typing import List, Dict
 
 #JAX Optimization
 from jax.example_libraries import optimizers
@@ -39,14 +39,14 @@ def parse_args():
     # File-paths
     parser.add_argument('--manifold', default="Sphere",
                         type=str)
-    parser.add_argument('--dim', default=[2,3,5,10,20,100],
-                        type=List)
+    parser.add_argument('--dim', default=2,
+                        type=int)
     parser.add_argument('--T', default=100,
                         type=int)
-    parser.add_argument('--scipy_methods', default=["BFGS"],
-                        type=List)
-    parser.add_argument('--jax_methods', default=["adam", "sgd"],
-                        type=List)
+    parser.add_argument('--scipy_methods', default=1,
+                        type=int)
+    parser.add_argument('--jax_methods', default=1,
+                        type=int)
     parser.add_argument('--jax_lr_rate', default=0.01,
                         type=float)
     parser.add_argument('--gc_lr_rate', default=1.0,
@@ -63,7 +63,7 @@ def parse_args():
                         type=int)
     parser.add_argument('--line_search_iter', default=100,
                         type=int)
-    parser.add_argument('--number_repeats', default=100,
+    parser.add_argument('--number_repeats', default=5,
                         type=int)
     parser.add_argument('--timing_repeats', default=5,
                         type=int)
@@ -99,6 +99,15 @@ def estimate_method(Geodesic, z0, zT, M):
     
     return method
 
+#%% Save times
+
+def save_times(methods:Dict, save_path:str)->None:
+    
+    with open(save_path, 'wb') as f:
+        pickle.dump(methods, f)
+    
+    return
+
 
 #%% Run Time code
 
@@ -106,95 +115,95 @@ def runtime_geodesics()->None:
     
     args = parse_args()
     
+    jax_methods = {"adam": optimizers.adam, "sgd": optimizers.sgd}
+    scipy_methods = ["BFGS"]
+    
     if not os.path.exists(args.save_path):
         os.makedirs(args.save_path)
         
     save_path = ''.join((args.save_path, args.manifold, '.pkl'))
     
-    output = {}
-    for dim in args.dim:
-        print(dim)
-        z0, zT, M = load_manifold(args.manifold, dim)
-        methods = {}
-        ## Gradient descent
-        print("Computing Gradient Descent")
-        Geodesic = GradientDescent(M = M,
-                                   init_fun=None,
-                                   lr_rate=args.gradient_lr_rate,
-                                   decay_rate=args.gradient_decay_rate,
-                                   T=args.T,
-                                   max_iter=args.max_iter,
-                                   tol=args.tol,
-                                   line_search_iter=args.line_search_iter
-                                   )
-        methods['gradient'] = estimate_method(jit(Geodesic), z0, zT, M)
-        ## Init Length
-        zt = Geodesic.init_fun(z0,zT,args.T)
-        init_length = M.length(zt)
-        init = {}
-        init['length'] = init_length
-        init['grad_norm'] = None
-        init['iterations'] = None
-        init['mu_time'] = None
-        init['std_time'] = None
-        init['tol'] = args.tol
-        init['max_iter'] = args.max_iter
-        methods['init'] = init
-        ## True Length
-        if hasattr(M, 'dist'):
-            true_dist = M.dist(z0,zT)
-            true = {}
-            true['length'] = true_dist
-            true['grad_norm'] = None
-            true['iterations'] = None
-            true['mu_time'] = None
-            true['std_time'] = None
-            true['tol'] = args.tol
-            true['max_iter'] = args.max_iter
-            methods['ground_truth'] = true
-        else:
-            true = {}
-            true['length'] = None
-            true['grad_norm'] = None
-            true['iterations'] = None
-            true['mu_time'] = None
-            true['std_time'] = None
-            true['tol'] = args.tol
-            true['max_iter'] = args.max_iter
-            methods['ground_truth'] = true
-        ## Geodesic Control
-        print("Computing Geodesic Control")
-        Geodesic = GC_LineSearch(M=M,
-                                 init_fun=None,
-                                 lr_rate=args.gc_lr_rate,
-                                 T=args.T,
-                                 decay_rate=args.gc_decay_rate,
-                                 tol=args.tol,
-                                 max_iter=args.max_iter,
-                                 line_search_iter=args.line_search_iter
-                                 )
-        methods['geodesic_control'] = estimate_method(jit(Geodesic), z0, zT, M)
-        ## JAX
+    z0, zT, M = load_manifold(args.manifold, args.dim)
+    methods = {}
+    ## Gradient descent
+    print("Computing Gradient Descent")
+    Geodesic = GradientDescent(M = M,
+                               init_fun=None,
+                               lr_rate=args.gradient_lr_rate,
+                               decay_rate=args.gradient_decay_rate,
+                               T=args.T,
+                               max_iter=args.max_iter,
+                               tol=args.tol,
+                               line_search_iter=args.line_search_iter
+                               )
+    methods['gradient'] = estimate_method(jit(Geodesic), z0, zT, M)
+    ## Init Length
+    zt = Geodesic.init_fun(z0,zT,args.T)
+    init_length = M.length(zt)
+    init = {}
+    init['length'] = init_length
+    init['grad_norm'] = None
+    init['iterations'] = None
+    init['mu_time'] = None
+    init['std_time'] = None
+    init['tol'] = args.tol
+    init['max_iter'] = args.max_iter
+    methods['init'] = init
+    save_times(methods, save_path)
+    ## True Length
+    if hasattr(M, 'dist'):
+        true_dist = M.dist(z0,zT)
+        true = {}
+        true['length'] = true_dist
+        true['grad_norm'] = None
+        true['iterations'] = None
+        true['mu_time'] = None
+        true['std_time'] = None
+        true['tol'] = args.tol
+        true['max_iter'] = args.max_iter
+        methods['ground_truth'] = true
+    else:
+        true = {}
+        true['length'] = None
+        true['grad_norm'] = None
+        true['iterations'] = None
+        true['mu_time'] = None
+        true['std_time'] = None
+        true['tol'] = args.tol
+        true['max_iter'] = args.max_iter
+        methods['ground_truth'] = true
+    save_times(methods, save_path)
+    ## Geodesic Control
+    print("Computing Geodesic Control")
+    Geodesic = GC_LineSearch(M=M,
+                             init_fun=None,
+                             lr_rate=args.gc_lr_rate,
+                             T=args.T,
+                             decay_rate=args.gc_decay_rate,
+                             tol=args.tol,
+                             max_iter=args.max_iter,
+                             line_search_iter=args.line_search_iter
+                             )
+    methods['geodesic_control'] = estimate_method(jit(Geodesic), z0, zT, M)
+    save_times(methods, save_path)
+    ## JAX
+    if args.jax_methods:
         print("Computing JAX")
-        for m in args.jax_methods:
-            if m == "adam":
-                optimizer = optimizers.adam
-            elif m=="sgd":
-                optimizer == optimizers.sgd
-            else:
-                raise ValueError("Invalid jax optimizer")
+        for opt in jax_methods:
             Geodesic = JAXOptimization(M = M,
                                        init_fun=None,
                                        lr_rate=args.jax_lr_rate,
-                                       optimizer=optimizer,
+                                       optimizer=jax_methods[opt],
                                        T=args.T,
                                        max_iter=args.max_iter,
                                        tol=args.tol
                                        )
-            methods[m] = estimate_method(jit(Geodesic), z0, zT, M)
-        ## Scipy
+            methods[opt] = estimate_method(jit(Geodesic), z0, zT, M)
+            save_times(methods, save_path)
+    ## Scipy
+    if args.scipy_methods:
         print("Computing Scipy")
-        for m in args.scipy_methods:
+        for m in scipy_methods:
             Geodesic = ScipyOptimization(M = M,
                                          T=args.T,
                                          tol=args.tol,
@@ -202,10 +211,7 @@ def runtime_geodesics()->None:
                                          method=m,
                                          )
             methods[m] = estimate_method(Geodesic, z0, zT, M)
-        output[str(dim)] = methods
-
-        with open(save_path, 'wb') as f:
-            pickle.dump(output, f)
+            save_times(methods, save_path)
     
     return
 
