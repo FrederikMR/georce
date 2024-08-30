7d27f4cfc15b26ea9b9d08d321da492771bb979a
Frederik-DTU <s164222@student.dtu.dk>
2024-08-30 10:57:16 +0200

    Update

--- submit_runtime.sh
+++ submit_runtime.sh
@@ -1,6 +1,6 @@
     #! /bin/bash
     #BSUB -q gpuv100
-    #BSUB -J ground_truth_REllipsoid10_100
+    #BSUB -J trust-exact_FH22_100
     #BSUB -n 4
     #BSUB -gpu "num=1:mode=exclusive_process"
     #BSUB -W 24:00
@@ -16,12 +16,12 @@
     module swap python3/3.10.12
     
     python3 runtime.py \
-        --manifold Ellipsoid \
-        --geometry Riemannian \
-        --dim 10 \
+        --manifold H2 \
+        --geometry Finsler \
+        --dim 2 \
         --T 100 \
         --v0 1.5 \
-        --method ground_truth \
+        --method trust-exact \
         --jax_lr_rate 0.01 \
         --tol 1e-4 \
         --max_iter 1000 \
