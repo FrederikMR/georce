21a0dbdbd5c1bfb4f5feb85349846c936576fe14
Frederik-DTU <s164222@student.dtu.dk>
2025-03-14 23:41:41 +0100

    Update

---
+++ submit_cl.sh
@@ -0,0 +1,25 @@
+    #! /bin/bash
+    #BSUB -q gpuv100
+    #BSUB -J T2
+    #BSUB -n 4
+    #BSUB -gpu "num=1:mode=exclusive_process"
+    #BSUB -W 24:00
+    #BSUB -R "span[hosts=1]"
+    #BSUB -R "rusage[mem=10GB]"
+    #BSUB -u fmry@dtu.dk
+    #BSUB -B
+    #BSUB -N
+    #BSUB -o sendmeemail/error_%J.out 
+    #BSUB -e sendmeemail/output_%J.err 
+    
+    module swap cuda/12.0
+    module swap cudnn/v8.9.1.23-prod-cuda-12.X
+    module swap python3/3.10.12
+    
+    python3 cut_locus.py \
+        --manifold T2 \
+        --n_grid 100 \
+        --runs 10 \
+        --seed 2712 \
+        --save_path cut_locus/ \
+    
