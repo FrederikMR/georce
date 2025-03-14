5dab51c32bbafc98fd652092c55c357f340814bb
Frederik Mbius Rygaard <fmry@pop-os.localdomain>
2025-03-14 22:56:59 +0100

    Update

--- cut_locus.py
+++ cut_locus.py
@@ -155,10 +155,7 @@ def compute_cut_locus()->None:
                          ))
     if os.path.exists(save_path):
         os.remove(save_path)
-        
-    from jax.lib import xla_bridge
-    print(xla_bridge.get_backend().platform)
-    return    
+
     if args.manifold == "Paraboloid":
         z0 = jnp.ones(2, dtype=jnp.float32)
         _, _, M, _ = load_manifold("Paraboloid", dim=2)
