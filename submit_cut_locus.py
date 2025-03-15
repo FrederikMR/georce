bcfbac02294a1c8508c892c492d39480d6c79149
Frederik Mbius Rygaard <fmry@pop-os.localdomain>
2025-03-15 01:37:02 +0100

    Update

--- submit_cut_locus.py
+++ submit_cut_locus.py
@@ -48,8 +48,8 @@ def generate_job(manifold):
     
     python3 cut_locus.py \\
         --manifold {manifold} \\
-        --n_grid 1000 \\
-        --runs 5 \\
+        --n_grid 200 \\
+        --runs 10 \\
         --seed 2712 \\
         --save_path cut_locus/ \\
     ''')
