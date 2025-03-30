6d912e0a1aba856a8b0a94e53913b3882c8a0c8b
Frederik-DTU <s164222@student.dtu.dk>
2025-03-30 09:19:29 +0200

    Updateø

--- submit_runtime.sh
+++ submit_runtime.sh
@@ -1,33 +1,36 @@
     #! /bin/bash
-    #BSUB -q hpc
-    #BSUB -J BVP_LSODA_RSphere1000_100
+    #BSUB -q gpuv100
+    #BSUB -J SparseRegNewton_Fmnist8_100
+    #BSUB -n 4
+    #BSUB -gpu "num=1:mode=exclusive_process"
     #BSUB -W 24:00
     #BSUB -R "span[hosts=1]"
-    #BSUB -R "select[model=XeonGold6226R]"
     #BSUB -R "rusage[mem=10GB]"
     #BSUB -u fmry@dtu.dk
     #BSUB -B
     #BSUB -N
     #BSUB -o sendmeemail/error_%J.out 
     #BSUB -e sendmeemail/output_%J.err 
-
+    
+    module swap cuda/12.0
+    module swap cudnn/v8.9.1.23-prod-cuda-12.X
     module swap python3/3.10.12
     
     python3 runtime.py \
-        --manifold Sphere \
-        --geometry Riemannian \
-        --dim 1000 \
+        --manifold mnist \
+        --geometry Finsler \
+        --dim 8 \
         --T 100 \
         --v0 1.5 \
-        --method BVP_LSODA \
+        --method SparseRegNewton \
         --jax_lr_rate 0.01 \
-        --tol 0.0001 \
+        --tol 0.001 \
         --max_iter 1000 \
         --line_search_iter 100 \
         --number_repeats 5 \
         --timing_repeats 5 \
         --seed 2712 \
-        --save_path timing_cpu/ \
+        --save_path timing_gpu/ \
         --svhn_path /work3/fmry/Data/SVHN/ \
         --celeba_path /work3/fmry/Data/CelebA/
     
